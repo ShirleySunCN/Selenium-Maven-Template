d28555f8fed2fc4d26e5eeca9599586382a92f81
Rophy Tsai <tsaiyl@tw.ibm.com>
2017-11-20 18:04:17 +0800

    only use firefox

--- src/test/java/com/lazerycode/selenium/config/DriverFactory.java
+++ src/test/java/com/lazerycode/selenium/config/DriverFactory.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import static com.lazerycode.selenium.config.DriverType.CHROME_HEADLESS;
+import static com.lazerycode.selenium.config.DriverType.FIREFOX;
 import static com.lazerycode.selenium.config.DriverType.valueOf;
 import static org.openqa.selenium.Proxy.ProxyType.MANUAL;
 
@@ -19,7 +19,7 @@ public class DriverFactory {
     private WebDriver webdriver;
     private DriverType selectedDriverType;
 
-    private final DriverType defaultDriverType = CHROME_HEADLESS;
+    private final DriverType defaultDriverType = FIREFOX;
     private final String browser = System.getProperty("browser", defaultDriverType.toString()).toUpperCase();
     private final String operatingSystem = System.getProperty("os.name").toUpperCase();
     private final String systemArchitecture = System.getProperty("os.arch");
