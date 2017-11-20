7fa0dfe421658308459dd17168c076fbeb27a26f
Rophy Tsai <tsaiyl@tw.ibm.com>
2017-11-20 18:18:25 +0800

    Rename sample test

--- src/test/java/com/lazerycode/selenium/tests/Test_01_010_010.java
+++ src/test/java/com/lazerycode/selenium/tests/Test_01_010_010.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class GoogleExampleIT extends DriverBase {
+public class Test_01_010_010 extends DriverBase {
 
     @Test
     public void googleCheeseExample() throws Exception {
