f2687bb5c8c2d12ce9938fbb1304e93a004f76a1
Rophy Tsai <tsaiyl@tw.ibm.com>
2017-11-24 18:33:00 +0800

    Add code example for taking screen shot in env IMAGE_DIR.

--- src/test/java/com/lazerycode/selenium/tests/MyTestCase.java
+++ src/test/java/com/lazerycode/selenium/tests/MyTestCase.java
@@ -1,12 +1,19 @@
 package com.lazerycode.selenium.tests;
 
-import com.lazerycode.selenium.DriverBase;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import com.lazerycode.selenium.DriverBase;
 
 /**
  * If you want to change class name,
@@ -14,6 +21,26 @@ import org.testng.annotations.Test;
  */
 public class MyTestCase extends DriverBase {
 
+	String getImageDir() {
+		String imageDir = System.getenv("IMAGE_DIR");
+		if (imageDir == null) {
+			imageDir = ".";
+		}
+		return imageDir;
+	}
+
+    void screenshot(WebDriver driver, String fileName) {
+        try {
+            FileOutputStream screenshotStream = new FileOutputStream(new File(getImageDir(), fileName));
+            screenshotStream.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
+            screenshotStream.close();
+        } catch (IOException unableToWriteScreenshot) {
+            System.err.println("Unable to write " + new File(getImageDir(), fileName).getAbsolutePath());
+            unableToWriteScreenshot.printStackTrace();
+        }
+    }
+
+    
     @Test
     public void test_01_010_010() throws Exception {
         // Create a new WebDriver instance
@@ -68,21 +95,20 @@ public class MyTestCase extends DriverBase {
 
         // Enter something to search for
         element.clear();
-        element.sendKeys("Milk!");
+        element.sendKeys("neko nanka");
 
         // Now submit the form. WebDriver will find the form for us from the element
         element.submit();
-
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
 
         // Google's search is rendered dynamically with JavaScript.
         // Wait for the page to load, timeout after 10 seconds
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("milk!");
+                return d.findElements(By.className("sb_pagN")).size() > 0;
             }
         });
+        
+        screenshot(driver, "02.010.020.png");
 
         // Should see: "cheese! - Google Search"
         System.out.println("Page title is: " + driver.getTitle());
